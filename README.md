6fff33718826261710199823ff8fd819168935af
Aniqsafr <211458@student.upm.edu.my>
2024-02-25 00:46:50 +0800

    remove api key and edit readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 This is Ai image generator website completely free! I will use huggingface api to create our own ai image generator app❗️
 
+link api 1:https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0
+link api 2: https://api-inference.huggingface.co/models/prompthero/openjourney"
+
+Vid ref: https://www.youtube.com/watch?v=KBbeUCQXBys
+
+WARNING!!!
+You need to get your own api key (in video we showed how!) and replace it in script.js file on line 1 :
+
 Here is project screenshot :
 
 ![screenshot](screenshot.jpg)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "API-KEY";
 
 const maxImages = 4; //images to generates for each prompt
 let selectedImageNumber = null;
@@ -24,58 +24,6 @@ function clearImageGrid(){
     const imageGrid = document.getElementById("image-grid");
     imageGrid.innerHTML = "";
 }
-
-//function to generate images
-// async function generateImages(input){
-//     disableGenerateButton();
-//     clearImageGrid();
-
-//     const loading = document.getElementById("loading");
-//     loading.style.display = "block";
-
-//     const imageUrls = [];
-
-//     for(let i = 0; i < maxImages; i++){
-//         //generate a random number between 1 and 100 and append it to the prompt
-//         const randomNumber = getRandomNumber(1, 10000);
-//         const prompt = `${input} ${randomNumber}`;
-//         //we added random number to prompt to create diffrent result
-//         const response = await fetch(
-//             "https://api-inference.huggingface.co/models/prompthero/openjourney",
-//             {
-//                 method: "POST",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                     "Authorization": `Bearer ${apiKey}`,
-//                 },
-//                 body: JSON.stringify({ input: prompt }),
-//             }
-//         );
-
-//         if(!response.ok){
-//             alert("failed to generate images!");
-//         }
-
-//         const blob = await response.blob();
-//         const imgUrl = URL.createObjectURL(blob);
-//         imageUrls.push(imgUrl);
-
-//         const img = document.createElement("img");
-//         img.src = imgUrl;
-//         img.alt = `art-${i + 1}`;
-//         img.onclick = () => downloadImage(imgUrl, i);
-//         document.getElementById("image-grid").appendChild(img);
-//     }
-
-//     loading.style.display = "none";
-//     enableGenerateButton();
-
-//     selectedImageNumber = null; //reset selectedImagesNumber
-// }
-// document.getElementById("generate").addEventListener('click', () =>{
-//     const input = document.getElementById("user-prompt").value;
-//     generateImages(input);
-// });
 
 async function generateImages(input) {
     disableGenerateButton();
